eab949f150f80c73f0fb06bd593551c05f8e60ed
free <csufuyi@126.com>
2015-10-24 16:44:42 +0800

    add bone

---
+++ _src/om2py0w/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#   头部声明
+'''文件说明
+作者信息
+版本自述
+    ...
+'''
+# 全局引用
+import sys
+# 全局变量
+PATH = "/path/2/work dir"
+# 函式撰写区
+def foo():
+    print 'hello world'
+    return None
+
+# 自检区
+if __name__ == '__main__':
+    if 1 != len(sys.argv) :
+        print '''Usage:
+$ python main.py
+        '''
+    else:
+        foo()
+
+
+
+
+
